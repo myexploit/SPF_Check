e37abe46a17da66a22e21022e7a61771f352ebcb
myexploit <nlines@nettitude.com>
2019-01-23 11:56:02 +0000

    Create SPF.sh

---
+++ SPF.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+read -p "Feed it your PS file:" Feed
+python sublist3r.py -v -o output1 -d $Feed
+sed 's/^www.//' output1 >> output2
+for LINE in `cat output2`
+do
+  echo "Got [$LINE]"
+nslookup -type=txt $LINE >> output3
+done
+grep spf1 output3
+rm output1
+rm output2
+rm output3
